fc0711927b48e9b95739e8fcab73f3e1ab5be8fa
Pulok Ahmed <pulokahmed07@gmail.com>
2023-05-27 17:55:51 +0600

    Final Commit

---
+++ MoviesApi/MoviesApi/Data/MoviesDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesApi.Data
+{
+    public class MoviesDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string MoviesCollectionName { get; set; } = string.Empty;
+    }
+}
